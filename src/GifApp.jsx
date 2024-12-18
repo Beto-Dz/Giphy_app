b3cd66e218ccb3b0f575c75e8ff15b8777d73996
Humbert Dominguez <humbertodelac13@gmail.com>
2024-12-17 18:52:10 -0600

    primer commit: primer layout de proyecto agregada

---
+++ src/GifApp.jsx
@@ -0,0 +1,15 @@
+export const GifApp = () => {
+  return (
+    <>
+      <header>
+        <h1>Giphy: Gif App</h1>
+        <form>
+          <input type="text" autoFocus placeholder="Busca imagenes..." />
+        </form>
+      </header>
+      <main>
+        
+      </main>
+    </>
+  );
+};
